20092034d01c8f2e5dc799aeceb88802ec4ca210
German G <german95guerrero@hotmail.com>
2023-05-02 09:22:53 -0500

    tarea 2js

---
+++ app.js
@@ -0,0 +1,15 @@
+const info = [
+  "German Guerrero",
+  "27",
+  true,
+  new Date("01-09-1995"),
+  {
+    name: "el arte de la guerra",
+    autor: "Sun Tzu",
+    fecha: "Siglo V a.C",
+    "fecha-publicacion": new Date("01-02-2009"), //Dos fecha, para practicar newDate()
+    url: "https://www.academiadeinversion.com/el-arte-de-la-guerra-de-sun-tzu-analisis-y-opinion-del-libro/",
+  },
+];
+
+console.log(info);
